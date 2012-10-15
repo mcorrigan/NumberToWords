28f52830de8836a76d2dd75c6991f6d732ad81f9
Michael Corrigan <mikec@basecampops.com>
2012-10-15 10:01:55 -0600

    Spelling Correct

--- NumberToWords.php
+++ NumberToWords.php
@@ -38,7 +38,7 @@ class NumberToWords {
         19 => 'nineteen',
         20 => 'twenty',
         30 => 'thirty',
-        40 => 'fourty',
+        40 => 'forty',
         50 => 'fifty',
         60 => 'sixty',
         70 => 'seventy',
